bfe4c6be5fa706a7db909d7a5a068f269e1b240d
Jakub Wejskrab <jakub.wejskrab@solitea.cz>
2023-02-10 17:27:54 +0100

    Added Requirements.R file for easier setup

---
+++ Requirements/Requirements.R
@@ -0,0 +1,12 @@
+#Nainstaluj R packages
+install.packages("dplyr", repos = "http://cran.us.r-project.org")
+install.packages("data.table", repos = "http://cran.us.r-project.org")
+install.packages("tidyr", repos = "http://cran.us.r-project.org")
+install.packages("zoo", repos = "http://cran.us.r-project.org")
+install.packages("readxl", repos = "http://cran.us.r-project.org")
+install.packages("janitor", repos = "http://cran.us.r-project.org")
+install.packages("stringr", repos = "http://cran.us.r-project.org") 
+install.packages("fuzzyjoin", repos = "http://cran.us.r-project.org")
+install.packages("lubridate", repos = "http://cran.us.r-project.org") 
+install.packages("rstudioapi", repos = "http://cran.us.r-project.org")
+install.packages("fuzzyjoin", repos = "http://cran.us.r-project.org")
